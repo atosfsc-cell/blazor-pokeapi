eccd162f4d9836424f2df9142ac187805f855fe0
atosfsc-cell <atosf.sc@gmail.com>
2025-10-29 20:28:09 -0300

    Add files via upload

---
+++ Models/Pokemon.cs
@@ -0,0 +1,11 @@
+namespace BlazorPokeApi.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
